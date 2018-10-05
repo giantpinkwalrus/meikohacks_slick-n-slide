88716e3d356f175988ddc809c4f5b65ceff64053
Sampo Lamminsalo <sampo.lamminsalo@meiko.fi>
2018-10-06 01:34:00 +0300

    car handling changes & handbreak

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -4,6 +4,17 @@ import Phaser from 'phaser'
 // import Car from '../sprites/Car'
 
 var cursors
+var keySpace
+const carSpecs = {
+  friction: 0.04,
+  mass: 40,
+  maxThrust: 0.035,
+  turning: 2.7,
+  handBreakModifier: 1.9
+}
+function clamp (min, max, value) {
+  return Math.min(Math.max(value, min), max)
+};
 export default class extends Phaser.Scene {
   constructor () {
     super({ key: 'GameScene' })
@@ -16,18 +27,23 @@ export default class extends Phaser.Scene {
   }
 
   update () {
-    if (cursors.left.isDown && cursors.up.isDown) {
-      Phaser.Physics.Matter.Matter.Body.setAngularVelocity(this.car.body, -0.02)
-      this.car.angle -= 2.3
-    } else if (cursors.right.isDown && cursors.up.isDown) {
-      Phaser.Physics.Matter.Matter.Body.setAngularVelocity(this.car.body, 0.02)
-      this.car.angle += 2.3
+    if (cursors.left.isDown && this.car.getData('engineThrust')) {
+      this.car.angle -= carSpecs.turning *
+        this.car.getData('engineThrust') *
+        (keySpace.isDown ? carSpecs.handBreakModifier : 1)
+    } else if (cursors.right.isDown && this.car.getData('engineThrust')) {
+      this.car.angle += carSpecs.turning *
+        this.car.getData('engineThrust') *
+        (keySpace.isDown ? carSpecs.handBreakModifier : 1)
     }
 
+    this.car.thrust(this.car.getData('engineThrust') * carSpecs.maxThrust)
     if (cursors.up.isDown) {
-      this.car.thrust(0.035)
-    } else if (cursors.down.isDown) {
-      this.car.thrustBack(0.025)
+      if (!keySpace.isDown) {
+        this.car.setData('engineThrust', clamp(0, 1, this.car.getData('engineThrust') + 0.1))
+      }
+    } else {
+      this.car.setData('engineThrust', clamp(0, 1, this.car.getData('engineThrust') - 0.005))
     }
   }
 
@@ -35,9 +51,11 @@ export default class extends Phaser.Scene {
     this.car = this.matter.add.image(400, 300, 'motor_bike')
 
     this.car.setAngle(0)
-    this.car.setFrictionAir(0.04)
-    this.car.setMass(40)
+    this.car.setFrictionAir(carSpecs.friction)
+    this.car.setMass(carSpecs.mass)
+    this.car.setData('engineThrust', 0)
     cursors = this.input.keyboard.createCursorKeys()
+    keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
     this.add.text(100, 100, 'Phaser 3 - ES6 - Webpack ', {
       font: '64px Bangers',
       fill: '#7744ff'
