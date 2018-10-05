61bcc2a4f6d282b492fbe7cca0772ec0c2f3e7cb
Sampo Lamminsalo <sampo.lamminsalo@meiko.fi>
2018-10-05 22:34:14 +0300

    rotating mushroom

--- src/config.js
+++ src/config.js
@@ -5,5 +5,13 @@ export default {
   parent: 'content',
   width: 800,
   height: 600,
-  localStorageName: 'phaseres6webpack'
+  localStorageName: 'phaseres6webpack',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      fps: 60,
+      gravity: { y: 0 },
+      debug: true
+    }
+  }
 }
--- src/sprites/Mushroom.js
+++ src/sprites/Mushroom.js
@@ -4,4 +4,7 @@ export default class extends Phaser.GameObjects.Sprite {
   constructor ({ scene, x, y, asset }) {
     super(scene, x, y, asset)
   }
+    update () {
+      console.log("update");
+    }
 }
