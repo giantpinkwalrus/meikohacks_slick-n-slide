c86c882c52a03a511261adb87ca2f5b32af8583b
Sampo Lamminsalo <sampo.lamminsalo@meiko.fi>
2018-10-06 17:40:33 +0300

    load track assets

--- src/scenes/Splash.js
+++ src/scenes/Splash.js
@@ -14,6 +14,9 @@ export default class extends Phaser.Scene {
     this.load.image('motor_bike', 'assets/images/vehicle_motorbike.png')
     this.load.image('vehicle_ufo', 'assets/images/vehicle_ufo.png')
     this.load.image('track_hacks_clean', 'assets/maps/track1/track_hacks_clean.png')
+    this.load.image('track2', 'assets/maps/track2/track2.png')
+    this.load.image('track3', 'assets/maps/track3/track3.png')
+    this.load.image('track_nascar', 'assets/maps/track4/track_nascar.png')
   }
 
   create () {
