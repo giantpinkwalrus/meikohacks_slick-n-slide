45e75d537daa86d1a80959d200fff35ace0bb5b0
Mikko Kokkoniemi <mikko.kokkoniemi@meiko.fi>
2018-10-17 15:16:18 +0300

    Some interactivity to menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -12,65 +12,60 @@ export default class extends Phaser.Scene {
   }
 
   startGame () {
-    this.scene.start('GameScene', { car: this.selection })
+    this.scene.start('GameScene', { car: this.selection.texture.key })
   }
 
   create () {
-    this.add.text(200, 200, 'Menju', {
-      font: '24px sans-serif',
-      fill: '#ff0000'
-    })
-
     // this.scene.start('MenuScene')
     const theme = window.game.sound.add('theme')
     theme.play()
     theme.loop = true
 
     this.add.tileSprite(400, 300, 800, 600, 'menu')
-    const button = this.add.sprite(400, 400, 'button')
+
+    const button = this.add.text(300, 400, 'Start the game', {
+      font: '32px Bangers',
+      fill: '#fff'
+    })
     button.setInteractive()
     button.on('pointerup', () => { this.startGame() })
+    button.on('pointerover', () => {
+      button.tint = 0xffff00
+      this.game.canvas.style.cursor = 'pointer'
+    })
+    button.on('pointerout', () => {
+      button.tint = 0xffffff
+      this.game.canvas.style.cursor = 'default'
+    })
 
-    const car = this.add.sprite(200, 200, 'vehicle_car')
-    const drag = this.add.sprite(230, 200, 'vehicle_dragcar')
-    const williams = this.add.sprite(260, 200, 'williams')
-    const toyota = this.add.sprite(290, 200, 'toyota')
-    const ferrari = this.add.sprite(320, 200, 'ferrari')
-    const bike = this.add.sprite(350, 200, 'motor_bike')
-    const ufo = this.add.sprite(380, 200, 'vehicle_ufo')
+    const cars = [
+      this.add.sprite(290, 300, 'vehicle_car'),
+      this.add.sprite(325, 300, 'vehicle_dragcar'),
+      this.add.sprite(360, 300, 'williams'),
+      this.add.sprite(395, 300, 'toyota'),
+      this.add.sprite(430, 300, 'ferrari'),
+      this.add.sprite(465, 300, 'motor_bike'),
+      this.add.sprite(500, 300, 'vehicle_ufo')
+    ]
 
-    this.selection = 'vehicle_car'
-    car.setInteractive()
-    drag.setInteractive()
-    williams.setInteractive()
-    toyota.setInteractive()
-    ferrari.setInteractive()
-    bike.setInteractive()
-    ufo.setInteractive()
-    car.on('pointerup', (event) => {
-      this.selection = 'vehicle_car'
-    })
-    drag.on('pointerup', (event) => {
-      this.selection = 'vehicle_dragcar'
-    })
-    williams.on('pointerup', (event) => {
-      this.selection = 'williams'
-    })
-    toyota.on('pointerup', (event) => {
-      this.selection = 'toyota'
-    })
-    ferrari.on('pointerup', (event) => {
-      this.selection = 'ferrari'
-    })
-    bike.on('pointerup', (event) => {
-      this.selection = 'motor_bike'
-    })
-    ufo.on('pointerup', (event) => {
-      this.selection = 'vehicle_ufo'
+    this.selection = cars[0]
+    cars.forEach(car => {
+      console.log(car)
+      car.setInteractive()
+      car.on('pointerup', () => {
+        this.selection.angle = 0
+        this.selection = car
+      })
+      car.on('pointerover', () => {
+        this.game.canvas.style.cursor = 'pointer'
+      })
+      car.on('pointerout', () => {
+        this.game.canvas.style.cursor = 'default'
+      })
     })
   }
 
   update () {
-
+    this.selection.angle += 1
   }
 }
